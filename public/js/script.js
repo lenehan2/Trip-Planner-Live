7203bd6535e2674454cebd9015f1e34742fa252c
Nick <npalenchar@gmail.com>
2015-11-25 15:27:55 -0500

    checkpoint before maps

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,9 @@ var markerArray = [];
 
 $(".panel-body").on('click','button',function(e){
 	
-	var $itineraryElement = $(this).siblings("select");
+	var $itineraryElement = $(this).siblings('select');
 	var elementClass = $itineraryElement.attr('class');
-	var $itemToAppend = "<div class='itinerary-item'><span class='title'>"+$itineraryElement.val()+"</span><button class='btn btn-xs btn-danger remove btn-circle'>x</button></div>";
+	var $itemToAppend = "<li><div class='itinerary-item'><span class='title'>"+$itineraryElement.val()+"</span><button class='btn btn-xs btn-danger remove btn-circle'>x</button></div></li>";
 	//console.log($itineraryElement.attr("class"));
 	$("ul."+$itineraryElement.attr("class")).append($itemToAppend);
 	//console.log(all_hotels)
@@ -17,7 +17,6 @@ $(".panel-body").on('click','button',function(e){
 	}
 	else if(elementClass === "restaurant"){
 		var latLongArray = searchHelper(all_restaurants,$itineraryElement);
-  	 	
 	}
 
 	var latLong = {lat: latLongArray[0], lng: latLongArray[1]};
@@ -25,11 +24,15 @@ $(".panel-body").on('click','button',function(e){
  //    if(!arraySearch(latLongArray,markerArray)){
 	// 	var idx = markerArray.indexOf(latLongArray);
 	// 	markerArray[idx] = null;
-	// 	marker.setMap(null);	   
+	// 	marker.setMap(null);
 	// }else{
 	// 	markerArray.push(latLongArray);
 	// }
  console.log(markerArray)
+});
+
+$("#itinerary").on('click', 'button', function(){
+	//remove
 })
 
 
@@ -38,6 +41,8 @@ var searchHelper = function(list,element){
    		return el.name === element.val();
    	})[0].place[0].location;
  };
+
+
 
  // var arraySearch = function(element,array){
  // 	for(var i = 0; i <array.length; i++){
